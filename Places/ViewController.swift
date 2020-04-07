29b1fe50684ad3f6679104d79116e705349ebde2
Aleksei Shipilov <maloverus@icloud.com>
2020-04-07 18:19:21 +0300

    Initial Commit

---
+++ Places/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Places
+//
+//  Created by Алексей Шипилов on 07.04.2020.
+//  Copyright © 2020 Aleksei Shipilov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
