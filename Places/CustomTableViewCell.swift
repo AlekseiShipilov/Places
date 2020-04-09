a27ed88e576a5cb1236e3ac46f973b158c0d417f
Aleksei Shipilov <maloverus@icloud.com>
2020-04-09 10:23:47 +0300

    1. Custom Table View Cell

---
+++ Places/CustomTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  CustomTableViewCell.swift
+//  Places
+//
+//  Created by Алексей Шипилов on 09.04.2020.
+//  Copyright © 2020 Aleksei Shipilov. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet var imageOfPlace: UIImageView!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var locationLabel: UILabel!
+    @IBOutlet var typeLabel: UILabel!
+    
+
+}
